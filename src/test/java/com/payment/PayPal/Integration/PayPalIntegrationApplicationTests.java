79fbc9baddfa268e43f5fde4a1d9370e535fdb10
deepanshupathak <deepanshupathak50@gmail.com>
2023-04-12 12:11:14 +0530

    first commit done

---
+++ src/test/java/com/payment/PayPal/Integration/PayPalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.PayPal.Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
